5f4e2ac0cf8bc84bd221f20be850a6eeb3c2dfa7
King of Mic <wooo.jackie@gmail.com>
2026-06-14 19:06:07 +0800

    Add mobile PWA install prompt: server.mjs

--- server.mjs
+++ server.mjs
@@ -11,6 +11,7 @@ const types = {
   ".css": "text/css; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".webmanifest": "application/manifest+json; charset=utf-8",
   ".xml": "application/xml; charset=utf-8",
   ".txt": "text/plain; charset=utf-8",
   ".png": "image/png",
